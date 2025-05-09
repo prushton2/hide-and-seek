8d7ae2f891826c6863281f8fc27d46c32ac04382
prushton2 <peterrushton418@gmail.com>
2025-05-09 13:19:27 -0400

    removed comment

--- frontend/src/map.tsx
+++ frontend/src/map.tsx
@@ -1,7 +1,6 @@
 import "./map.css";
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup, Polyline, CircleMarker} from 'react-leaflet';
-// import { Icon } from 'leaflet';
 
 function Map() {
   return (
@@ -11,5 +10,4 @@ function Map() {
   );
 }
 
-// center={[42.36041830331139, -71.0580009624248]}
 export default Map;

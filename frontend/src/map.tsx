f595a506c15729f087e5ac150a4852b4c58ae2ed
prushton2 <peterrushton418@gmail.com>
2025-05-10 23:35:54 -0400

    changed the coordinates

--- frontend/src/map.tsx
+++ frontend/src/map.tsx
@@ -1,11 +1,19 @@
 import "./map.css";
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, Marker, Popup, Polyline, CircleMarker} from 'react-leaflet';
+import { MapContainer, TileLayer, Polygon, Popup, Polyline, CircleMarker} from 'react-leaflet';
 
 function Map() {
+
+  const purpleOptions = { color: 'purple' };
+
+  const polygon = [
+    [42.203745, -70.983822], [42.203745, -71.269668], [42.526848, -71.269668], [42.526848, -71.151812]
+  ];
+
   return (
     <MapContainer center={[42.36041830331139, -71.0580009624248]} zoom={13} className='map'>
       <TileLayer url="https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png" />
+      <Polygon pathOptions={purpleOptions} positions={polygon} />
     </MapContainer>
   );
 }

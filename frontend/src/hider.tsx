30f87ab3f5a06afa2514dc851e5f9abd10001381
prushton2 <peterrushton418@gmail.com>
2025-05-16 18:18:53 -0400

    added map for hider

--- frontend/src/hider.tsx
+++ frontend/src/hider.tsx
@@ -1,26 +1,52 @@
-import { useState } from 'react'
-import './App.css'
+import './seeker.css'
+import { useEffect, useState } from 'react'
+import Map from './map.tsx'
+import { update } from "./API.tsx"
+
+
 
 function Hider() {
-  const [count, setCount] = useState(0)
+  const [shapes, setShapes] = useState<number[][][]>([[[]]]);
+  const [hider, setHider] = useState<number[]>([0,0]);
+  const [seeker, setSeeker] = useState<number[]>([0,0]);
+
+  async function updateQuestions() {
+    let response = await update()
+
+    let newShapes: number[][][] = []
+    if(response.shapes == null) {
+      response.shapes = [[]]
+    }
+    for(let i = 0; i < response.shapes.length; i++) {
+      newShapes.push([])
+      for(let j = 0; j < response.shapes[i].length; j++) {
+        newShapes[i][j] = [response.shapes[i][j].X, response.shapes[i][j].Y]
+      }
+    }
+    // console.log(newShapes)
+    setShapes(newShapes)
+    try {
+      setHider([response.hiderpos.X, response.hiderpos.Y])
+    } catch {
+      setHider([0,0])
+    }
+
+    try {
+      setSeeker([response.seekerpos.X, response.seekerpos.Y])
+    } catch {
+      setSeeker([0,0])
+    }
+  }
+
+  useEffect(() => {
+    updateQuestions()
+    setInterval(updateQuestions, 15000);
+  }, [])
 
   return (
-    <>
-      <div>
-      </div>
-      <h1>Hider</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <Map shapes={shapes} hider={hider} seeker={seeker}/>
+    </div>
   )
 }
 

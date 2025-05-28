ba7457dd1817f359a437d94c6eb5f5eed6596bcd
prushton2 <peterrushton418@gmail.com>
2025-05-27 20:30:52 -0400

    rewrote questions

--- frontend/src/lib/questions.tsx
+++ frontend/src/lib/questions.tsx
@@ -1,72 +1,225 @@
-export interface question {
+import { useState, type JSX } from "react";
+import { ask } from "./API";
+
+export function Questions({callback}: {callback: (question: string) => void}) {
+    const [state, setState] = useState<string>("showCategories"); // state (showCategories, showQuestions, showQuestion)
+    const [info, setInfo] = useState<string[]>([])                  // info about the state (question being shown, category being shown, etc)
+
+    function renderCategories() {
+        let arr: JSX.Element[] = []
+        
+        Object.entries(questionCategories).forEach((category: [string, Category]) => {
+            arr.push(
+                <button key={category[0]}
+                    className="questionCategory" 
+                    onClick={() => {setState("showQuestions"); setInfo([category[0]])}}>
+                    {category[1].name}
+                </button>
+            )
+        })
+        return arr
+    }
+
+    function renderQuestions() {
+        
+        let arr: JSX.Element[] = Object.entries(questionsMap)
+            .filter((e: [string, Question]) => e[0].startsWith(info[0]))
+            .map((question: [string, Question]) => {
+                return <button key={question[1].id} className="question" onClick={(e) => {setState("showQuestion"); setInfo([question[0], info[0]])}}>
+                    {question[1].name}
+                </button>
+            })
+
+        arr.unshift(
+            <button key="back" className="question" onClick={() => { setState("showCategories"); setInfo([]); }}>
+                <div className="questionName">Back</div>
+            </button>
+        );
+            
+        return <>
+            {questionCategories[info[0]].desc}
+            <div className="questionContainer">    
+                {arr}
+            </div>
+        </>
+    }
+
+    function renderQuestion() {
+        let description = questionCategories[info[1]].desc
+
+        questionsMap[info[0]].replaceString.forEach((e) => {
+            description = description.replace("__", e)
+        })
+
+        return <div>
+            <button onClick={(e) => { setState("showQuestions"); setInfo([info[1]]) }}>Back</button>
+            <h2>{questionCategories[info[1]].name}: {questionsMap[info[0]].name}</h2>
+            {description}
+            <br />
+            <br />
+            <br />
+            <button
+                onClick={async(e) => { await ask(info[0]); callback(info[0]); setState("showCategories"); setInfo([]) }}
+            >Ask</button>
+        </div>
+    }
+
+    switch(state) {
+        case "showCategories":
+            return renderCategories()
+        case "showQuestions":
+            return renderQuestions()
+        case "showQuestion":
+            return renderQuestion()
+    }
+    return <></>
+}
+
+export interface Question {
     name: string,
     id:   string,
     icon: string
+    replaceString: string[]
 };
 
-export const questions: { [key: string]: question } = {
+export interface Category {
+    name: string
+    desc: string
+}
+
+const questionCategories: { [key: string]: Category } = {
+    "matching": {
+        name: "Matching",
+        desc: "Is your nearest __ the same as mine?"
+    },
+    "measure": {
+        name: "Measuring",
+        desc: "Are you closer or farther from your nearest __ than me?"
+    },
+    "thermometer": {
+        name: "Thermometer",
+        desc: "Ive traveled __, am I closer or farther to you?"
+    },
+    "tentacles": {
+        name: "Tentacles",
+        desc: "Out of all the __ within __ of me, which one are you closest to?"
+    },
+    "radar": {
+        name: "Radar",
+        desc: "Are you within __ of me?"
+    }
+}
+
+const questionsMap: { [key: string]: Question } = {
+
+    // Matching
+    
     "matching-subway": {
-        name: "Same Subway Station",
+        name: "Subway Station",
         id: "matching-subway",
-        icon: ""
+        icon: "",
+        replaceString: ["subway station"]
     },
     "matching-light_rail": {
-        name: "Same Light Rail Stop",
+        name: "Light Rail Stop",
         id: "matching-light_rail",
-        icon: ""
+        icon: "",
+        replaceString: ["light rail station"]
     },
-    "matching-McDonald's": {
-        name: "Same McDonald's",
-        id: "matching-McDonald's",
-        icon: ""
+    "matching-raisingcanes": {
+        name: "Raising Cane's",
+        id: "matching-raisingcanes",
+        icon: "",
+        replaceString: ["Raising Cane's"]
+    },
+    "matching-hospital": {
+        name: "Hospital",
+        id: "matching-hospital",
+        icon: "",
+        replaceString: ["hospital"]
     },
 
+    // Measure
+
+    "measure-subway": {
+        name: "Subway Station",
+        id: "measure-subway",
+        icon: "",
+        replaceString: ["subway station"]
+    },
+    "measure-raisingcanes": {
+        name: "Raising Cane's",
+        id: "measure-raisingcanes",
+        icon: "",
+        replaceString: ["Raising Cane's"]
+    },
     "measure-hospital": {
         name: "Hospital",
         id: "measure-hospital",
-        icon: ""
+        icon: "",
+        replaceString: ["hospital"]
+    },
+    "measure-airport": {
+        name: "Airport",
+        id: "measure-airport",
+        icon: "",
+        replaceString: ["airport"]
     },
     
+    // Thermometer
+
     "thermometer-4min": {
         name: "4 min",
         id: "thermometer-4min",
-        icon: ""
+        icon: "",
+        replaceString: ["for 4 minutes"]
     },
+
+    // Tentacles
     
     "tentacles-McDonald's": {
         name: "McDonald's",
         id: "tentacles-McDonald's",
-        icon: ""
+        icon: "",
+        replaceString: ["McDonald's", "1 mile"]
     },
     "tentacles-Wendy's": {
         name: "Wendy's",
         id: "tentacles-Wendy's",
-        icon: ""
+        icon: "",
+        replaceString: ["Wendy's", "1 mile"]
     },
     "tentacles-burger": {
         name: "Burger Restaurant",
         id: "tentacles-burger",
-        icon: ""
+        icon: "",
+        replaceString: ["burger restaurants", "1 mile"]
     },
     
+    // Radar
+
     "radar-0.5mi": {
         name: "Half Mile Radar",
         id: "radar-0.5mi",
-        icon: ""
+        icon: "",
+        replaceString: ["half a mile"]
     },
     "radar-1mi": {
         name: "1 Mile Radar",
         id: "radar-1mi",
-        icon: ""
+        icon: "",
+        replaceString: ["1 mile"]
     },
     "radar-2mi": {
         name: "2 Mile Radar",
         id: "radar-2mi",
-        icon: ""
+        icon: "",
+        replaceString: ["2 miles"]
     },
     "radar-3mi": {
         name: "3 Mile Radar",
         id: "radar-3mi",
-        icon: ""
+        icon: "",
+        replaceString: ["3 miles"]
     }
 };
--- frontend/src/seeker.tsx
+++ frontend/src/seeker.tsx
@@ -1,75 +1,9 @@
 import './seeker.css'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, type JSX } from 'react'
 import Map from './components/map.tsx'
-import { questions, type question } from './lib/questions.tsx'
-import { update, ask } from "./lib/API.tsx"
+import { Questions } from './lib/questions.tsx'
+import { update } from "./lib/API.tsx"
 import type { Shapes, Vector2 } from './lib/interface.ts'
-
-function Questions({callback}: {callback: (question: string) => void}) {
-  const [questionCategory, setQuestionCategory] = useState<string>("");
-
-  function renderQuestion(question: question) {
-    return <button className="question" key={question.id} onClick={async(e) => {
-        if(question.id == "back") {
-          setQuestionCategory("")
-        } else {
-          await ask(question.id)
-          callback(question.id)
-        }
-      }}>
-      <div className="questionName">{question.name}</div>
-    </button>
-  }
-  function renderQuestionBox() {
-
-    if(questionCategory == "") {
-      return <div className="questionCategoryContainer">
-          <button 
-            className="questionCategory" 
-            onClick={() => setQuestionCategory("matching")}>
-            Matching
-          </button>
-          <button 
-            className="questionCategory" 
-            onClick={() => setQuestionCategory("measuring")}>
-            Measuring
-          </button>
-          <button 
-            className="questionCategory" 
-            onClick={() => setQuestionCategory("thermometer")}>
-            Thermometer
-          </button>
-          <button 
-            className="questionCategory" 
-            onClick={() => setQuestionCategory("radar")}>
-            Radar
-          </button>
-          <button 
-            className="questionCategory" 
-            onClick={() => setQuestionCategory("tentacles")}>
-            Tentacles
-          </button>
-          <button 
-            className="questionCategory" 
-            onClick={() => setQuestionCategory("photos")}>
-            Photos
-          </button>
-        </div>
-    } else {
-      return <div className="questionContainer">
-        {
-          [renderQuestion({name: "Back", id: "back", icon: ""} as question)].concat(
-            Object.entries(questions)
-              .filter((e: [string, question]) => e[0].startsWith(questionCategory))
-              .map((e: [string, question]) => {return renderQuestion(e[1])})
-          )
-        }
-      </div>
-    }
-  }
-
-  return renderQuestionBox()
-}
 
 function Seeker() {
   const [shapes, setShapes] = useState<Shapes>();

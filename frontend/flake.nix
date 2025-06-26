f47933df99601ba8200a22569bbb8221502e215d
prushton2 <peterrushton418@gmail.com>
2025-06-26 11:29:53 -0400

    build changes

--- frontend/flake.nix
+++ frontend/flake.nix
@@ -10,7 +10,7 @@
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in
     {
-      packages.x86_64-linux.hide-and-seek = pkgs.buildNpmModule {
+      packages.x86_64-linux.hide-and-seek = pkgs.buildNpmPackage {
         pname = "hide-and-seek";
         version = "0.1.0";
         src = ./.;
